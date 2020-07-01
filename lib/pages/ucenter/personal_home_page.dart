b76405ca6fef14bd5fa54c9d1b11264b3064da5b
yiyuanyan <yiyuanhanly@126.com>
2020-07-01 23:17:03 +0800

    开始制作个人主页

---
+++ lib/pages/ucenter/personal_home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+class PersonalHomePage extends StatefulWidget {
+  @override
+  _PersonalHomePageState createState() => _PersonalHomePageState();
+}
+
+class _PersonalHomePageState extends State<PersonalHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+
+        ],
+      ),
+    );
+  }
+}
